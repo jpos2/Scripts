02cc20bbf2acb14b88ecf9c38500691e8f613fab
Jean Pierre <jpos@cin.ufpe.br>
2016-05-12 22:29:01 -0300

    new file:   C++/helloworld.cpp 	new file:   C++/helloworld.sh 	new file:   C++/meunome.cpp 	new file:   C++/meunome.sh 	new file:   C++/teste 	new file:   JavaScript/helloword.js 	new file:   JavaScript/index.html 	new file:   Python/Huxley/242.py

---
+++ C++/helloworld.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+//Build by jpos2@cin.ufpe.br
+
+int main()
+{
+    printf("Hello World\n");
+}
---
+++ C++/meunome.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <bits/stdc++.h>
+//Build by jpos2@cin.ufpe.br
+
+using namespace std;
+
+int  main()
+{
+    
+    string name [30];
+    int age;
+    float height;
+    
+    printf("Informe seu nome, sua idade e sua altura\n");
+    scanf("%s %d %f", &name, &age, &height);
+    printf("Seu nome e: %s, voce tem %i anos e mede %f de altura.\n", name, age, height);
+    
+}
